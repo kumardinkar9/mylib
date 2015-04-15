04b75e8b9b329947871d06e6b85b2242dd900ca6
Saurabh Vijayvargiya <sorabh.v6@gmail.com>
2015-04-15 11:52:43 +0530

    Created utility_function

---
+++ utility_functions.js
@@ -0,0 +1,15 @@
+function Validate() {
+          var mobile = document.getElementById("mobile").value;
+          var pattern = /^(((\+){0,1}91|0)(\s){0,1}(\-){0,1}(\s){0,1}){0,1}[7-9][0-9](\s){0,1}(\-){0,1}(\s){0,1}[1-9]{1}[0-9]{7}$/;
+          if (pattern.test(mobile)) {
+              return true;
+          }
+          alert("It is not valid mobile number, input 10 digits number!");
+          return false;
+      }
+      $( "form" ).submit(function( event ) {
+        if ( Validate() ) {
+        return;
+      }
+        event.preventDefault();
+      });
