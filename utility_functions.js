2c802d739739f057e98cc9469cd7ef270c82b144
Saurabh Vijayvargiya <sorabh.v6@gmail.com>
2015-04-15 11:56:05 +0530

    changed the function name to MobileNumberValidate

--- utility_functions.js
+++ utility_functions.js
@@ -1,4 +1,4 @@
-function Validate() {
+function MobileNumberValidate() {
           var mobile = document.getElementById("mobile").value;
           var pattern = /^(((\+){0,1}91|0)(\s){0,1}(\-){0,1}(\s){0,1}){0,1}[7-9][0-9](\s){0,1}(\-){0,1}(\s){0,1}[1-9]{1}[0-9]{7}$/;
           if (pattern.test(mobile)) {
