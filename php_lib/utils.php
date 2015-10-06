86ffa4a345ea886ce1200ea714d56e72fc9b18af
Saurabh Vijayvargiya <sorabh.v6@gmail.com>
2015-10-06 07:12:46 +0000

    Created php folder and utils.php

---
+++ php_lib/utils.php
@@ -0,0 +1,11 @@
+/*
+ * This file contains a Utils class.
+ * This class contains various utility functions like db connection, file upload, mail function, etc.
+ * Description is provided before every functions along with required parameters and its return entity.
+ */
+
+ class Utils{
+
+
+
+ }
