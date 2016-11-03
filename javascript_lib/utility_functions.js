e7c2fa6f662ba0d2ba345e4bde83336118b86374
Saurabh Vijayvargiya <saurabh.vijayvargiya@io-media.com>
2016-11-03 16:42:57 +0530

    added method for removing duplicates

--- javascript_lib/utility_functions.js
+++ javascript_lib/utility_functions.js
@@ -1,24 +1,30 @@
 function MobileNumberValidate() {
-          var mobile = document.getElementById("mobile").value;
-          var pattern = /^(((\+){0,1}91|0)(\s){0,1}(\-){0,1}(\s){0,1}){0,1}[7-9][0-9](\s){0,1}(\-){0,1}(\s){0,1}[1-9]{1}[0-9]{7}$/;
-          if (pattern.test(mobile)) {
-              return true;
-          }
-          alert("It is not valid mobile number, input 10 digits number!");
-          return false;
-      }
-$( "form" ).submit(function( event ) {
-  if ( Validate() ) {
-  return;
+  var mobile = document.getElementById("mobile").value;
+  var pattern = /^(((\+){0,1}91|0)(\s){0,1}(\-){0,1}(\s){0,1}){0,1}[7-9][0-9](\s){0,1}(\-){0,1}(\s){0,1}[1-9]{1}[0-9]{7}$/;
+  if (pattern.test(mobile)) {
+    return true;
+  }
+  alert("It is not valid mobile number, input 10 digits number!");
+  return false;
 }
-  event.preventDefault();
-)};
+$("form").submit(function(event) {
+    if (Validate()) {
+      return;
+    }
+    event.preventDefault();
+  )
+};
 
 // Get current year
 var currentYear = new Date().getFullYear();
 
 // Get timestamp for some other date
-var otherDateTimestamp = Date.parse(new Date('15 Mar'+new Date().getFullYear()))/1000;
+var otherDateTimestamp = Date.parse(new Date('15 Mar' + new Date().getFullYear())) / 1000;
 
 // Get current timestamp in seconds
-var currentTimestamp = jQuery.now()/1000;
+var currentTimestamp = jQuery.now() / 1000;
+
+// Remove duplicate values from array
+var unique = duplicateValues.filter(function(elem, index, self) {
+  return index == self.indexOf(elem);
+}).sort(); // sort() is sorting the array after removing duplicates
